d7f47ce578bb035531746d2c9e44853b78aa958c
sik2 <bickwang123@gmail.com>
2025-01-14 02:15:52 +0900

    ci/cd

--- src/main/java/com/ll/demo03/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/demo03/domain/home/home/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "Hello, World!!!!!, on " + siteName;
+        return "Hello, World!!!!! complete CI/CD, on " + siteName;
     }
 
     @GetMapping("/secretKey")
